ae3f8de8cf0ccb2660ee8526479c0b29967f43aa
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-17 23:49:14 -0400

    FormComponent references definitions from @codotype/types

--- generator/resource/templates/form-component.js
+++ generator/resource/templates/form-component.js
@@ -28,7 +28,6 @@ class <%- schema.class_name %>Form extends Component {
     return (
       <div className='row'>
         <%_ for (let attr of schema.attributes) { _%>
-        <%_ if (attr.datatype === 'RELATION' && attr.datatypeOptions.relationType === 'OWNS_MANY') continue _%>
         <div className="col-lg-6">
           <div className="form-group">
             <label className='mb-0'>
@@ -36,26 +35,20 @@ class <%- schema.class_name %>Form extends Component {
               <% if (attr.required) { %><span className='text-danger'>*</span><% } %>
             </label>
             <small className="form-text text-muted mb-2"><%= attr.help %></small>
-          <%_ if (attr.datatype === 'BOOL') { _%>
+          <%_ if (attr.datatype === DATATYPE_BOOLEAN) { _%>
             <input type="checkbox" className="form-control" checked={Boolean(this.getModel('<%- attr.identifier %>'))} onChange={(e) => this.setModel('<%- attr.identifier %>', e.target.checked)} />
-          <%_ } else if (attr.datatype === 'TEXT') { _%>
+          <%_ } else if (attr.datatype === DATATYPE_STRING) { _%>
             <input type="text" className="form-control" placeholder="<%= attr.label %>" value={String(this.getModel('<%- attr.identifier %>'))} onChange={(e) => this.setModel('<%- attr.identifier %>', e.target.value)} />
-          <%_ } else if (attr.datatype === 'NUMBER') { _%>
+          <%_ } else if (attr.datatype === DATATYPE_NUMBER) { _%>
             <input type="number" className="form-control" placeholder="<%= attr.label %>" value={String(this.getModel('<%- attr.identifier %>'))} onChange={(e) => this.setModel('<%- attr.identifier %>', e.target.value)} />
-          <%_ } else if (attr.datatype === 'DATE') { _%>
+          <%_ } else if (attr.datatype === DATATYPE_DATE) { _%>
             <input type="date" className="form-control" placeholder="<%= attr.label %>" value={String(this.getModel('<%- attr.identifier %>')).split('Z')[0]} onChange={(e) => this.setModel('<%- attr.identifier %>', e.target.value + 'Z')} />
-          <%_ } else if (attr.datatype === 'TIME') { _%>
+          <%_ } else if (attr.datatype === DATATYPE_TIME) { _%>
             <input type="time" className="form-control" placeholder="<%= attr.label %>" value={String(this.getModel('<%- attr.identifier %>')).split('Z')[0]} onChange={(e) => this.setModel('<%- attr.identifier %>', e.target.value + 'Z')} />
-          <%_ } else if (attr.datatype === 'DATETIME') { _%>
+          <%_ } else if (attr.datatype === DATATYPE_DATETIME) { _%>
             <input type="datetime-local" className="form-control" placeholder="<%= attr.label %>" value={String(this.getModel('<%- attr.identifier %>')).split('Z')[0]} onChange={(e) => this.setModel('<%- attr.identifier %>', e.target.value + 'Z')} />
-          <%_ } else if (attr.datatype === 'JSON') { _%>
+          <%_ } else if (attr.datatype === DATATYPE_JSON) { _%>
             <textarea className="form-control" placeholder="<%= attr.label %>" onChange={(e) => this.setModel('<%- attr.identifier %>', JSON.parse(e.target.value))}>{ JSON.stringify(this.getModel('<%- attr.identifier %>'), null, 2) }</textarea>
-          <%_ } else if (attr.datatype === 'RELATION') { _%>
-
-          <%_ } if (attr.datatypeOptions.relationType === 'BELONGS_TO') { _%>
-            { /* TODO add the select */ }
-          <%_ } else if (attr.datatypeOptions.relationType === 'HAS_MANY') { _%>
-            { /* TODO add the select */ }
           <%_ } _%>
           </div>
         </div>
